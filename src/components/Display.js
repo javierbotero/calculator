0163cefe5375d421d17b7259995c31d7d7914ce3
Javier Botero <javierbotero10271@gmail.com>
2021-01-16 15:18:52 -0500

    Improve the render of display showing properties if they aren't null

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,7 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Display({ result }) {
-  return <div>{result === null ? 0 : result}</div>;
+  let str;
+  if (result.total === null) {
+    str = '0';
+  } else {
+    str = `${result.total}
+ ${result.operation ? result.operation : ''}
+ ${result.next ? result.next : ''}`;
+  }
+  console.log(result);
+  return <div>{str}</div>;
 }
 
 Display.defaultProps = {
